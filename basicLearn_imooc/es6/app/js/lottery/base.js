298e16b7bcb9ce105eb1483abe09d246ebdf2f21
Amy <657074944@qq.com>
2017-08-30 22:15:14 +0800

    ----------

--- basicLearn_imooc/es6/app/js/lottery/base.js
+++ basicLearn_imooc/es6/app/js/lottery/base.js
@@ -162,16 +162,58 @@ class Base{
      * [addCode 添加号码]
      */
     addCode(){
-
+        let self=this;
+        let $active=$(".boll-list .btn-boll-active").text().match(/\d{2}/g);
+        let active=$active?$active.length:0;
+        let count=self.computeCount(active,self.cur_play);
+        if(count){
+            self.addCodeItem($active.join(''),self.cur_play,self.play_list.get(self.cur_play).name,count);
+        }
     }
     /**
      * [addCode 添加单次号码]
      */
     addCodeItem(){
-
+        let self=this;
+        const tpl=`
+        <li codes="${type}|${code}" bonus="${count*2}" count="${count}">
+          <div class="code">
+             <b>${typeName}${count>1?'复式':'单式'}</b>
+             <b class="em">${code}</b>
+             [${count}注，<em class="code-list-money">${count*2}</em>元]
+          </div>
+        </li>
+        `;
+        $(self.cart_el).append(tpl);
+        self.getTotal();
     }
 
     getCount(){
+        let self=this;
+        let active=$('.boll-list .btn-boll-active').length;
+        let count=self.computeCount(active,self.cur_play);
+        let range=self.computeBonus(active,self.cur_play);
+        let money=count*2;
+        let win1=range[0]-money;
+        let win2=range[1]-money;
+        let tpl;
+        let c1=(win1<0&&win2<0)?Math.abs(win1):win1;
+        let c2=(win1<0&&win2<0)?Math.abs(win2):win2;
+        if(count===0){
+            tpl=`您选了<b class="red">${count}</b>注，共<b class="red">${count*2}</b>元`;
+        }else if(range[0]===range[1]){
+            tpl=`您选了<b>${count}</b>注,共<b>${count*2}</b>元 <em>若中奖，奖金：
+            <strong class="red">${range[0]}</strong>至<strong class="red">${range[1]}</strong>元，
+            您将${(win1<0&&win2<0)?'亏损':'盈利'}
+            <strong class="${win1>=0?'red':'green'}">${Math.abs(win1)}</strong>元</em>`;
+        }else{
+            tpl=`您选了<b>${count}</b>注,共<b>${count*2}</b>元 <em>若中奖，奖金：
+            <strong class="red">${range[0]}</strong>至<strong class="red">${range[1]}</strong>元，
+            您将${(win1<0&&win2<0)?'亏损':'盈利'}
+            <strong class="${win1>=0?'red':'green'}">${c1}</strong>至<strong class="${win2>=0?'red':'green'}">${c1}</strong>元</em>`;
+        }
+
+        $('.sel_info').html(tpl);
 
     }
 
@@ -179,7 +221,12 @@ class Base{
      * ［getTotal 计算所有金额］
      */
     getTotal(){
-
+        let count=0;
+        $('.codelist li').each(function(index,item){
+            count+=$(item).attr(count)*1;
+        });
+        $("#count").text(count);
+        $("#money").text(count*2);
     }
 
     /**
@@ -187,7 +234,14 @@ class Base{
      * @param num
      */
     getRandom(num){
-
+        let arr=[],index;
+        let number=Array.from(this.number);
+        while(num--){
+            index=Number.parseInt(Math.random()*number.length);
+            arr.push(number[index]);
+            number.splice(index,1);
+        }
+        return arr.join(' ');
     }
 
     /**
@@ -195,7 +249,17 @@ class Base{
      * @param e
      */
      getRandomCode(e){
-
+        e.preventDefault();
+        let num= e.currentTarget.getAtrribute('count');
+        let play=this.cur_play.match(/\d+/g)[0];
+        let self=this;
+        if(num==='0'){
+            $(self.cart_el).html('');
+        }else{
+            for(let i=0;i<num;i++){
+                self.addCodeItem(self.getRandom(play),self.cur_play,self.play_list.get(self.cur_play).name,1);
+            }
+        }
      }
 
     export default Base;
